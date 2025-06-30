5c571f91b10d2522f7eb37e77a3d48a83376a132
closeLIDER995 <abduvaliyevabdulloh881@>
2025-06-30 09:51:23 +0300

    finishing

--- src/Controller/authCtrl.js
+++ src/Controller/authCtrl.js
@@ -1,23 +1,29 @@
-const User = require('../Model/UserModel');
 const bcrypt = require('bcryptjs');
 const JWT = require('jsonwebtoken');
+const User = require('../models/UserModel');
 
 const authCtrl = {
   signup: async (req, res) => {
     try {
-      const { username, surname, email, password } = req.body;
+      const { username, email, password, surname, job, hobby, role = 'user' } = req.body;
 
-      if (!username || !surname || !email || !password) {
-        return res.status(400).json({ message: "Barcha maydonlarni to‘ldiring" });
+      if (!username || !email || !password || !surname) {
+        return res.status(400).json({ message: "Barcha qatorlarni to‘ldiring" });
       }
 
-      const userExists = await User.findOne({ email });
-      if (userExists) return res.status(400).json({ message: "Bu email allaqachon mavjud" });
+      const [emailExists, usernameExists] = await Promise.all([
+        User.findOne({ email }),
+        User.findOne({ username }),
+      ]);
 
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const newUser = new User({ username, surname, email, password: hashedPassword });
+      if (emailExists) return res.status(403).json({ message: "Bu email allaqachon mavjud" });
+      if (usernameExists) return res.status(403).json({ message: "Bu username allaqachon mavjud" });
 
+      const newUser = new User({ username, email, password, surname, job, hobby, role });
       await newUser.save();
+
+      const userData = newUser.toObject();
+      delete userData.password;
 
       const token = JWT.sign(
         { id: newUser._id, role: newUser.role },
@@ -25,41 +31,48 @@ const authCtrl = {
         { expiresIn: '12h' }
       );
 
-      const userData = newUser.toObject();
-      delete userData.password;
-
-      res.status(201).json({ message: 'Ro‘yxatdan o‘tildi', userId: newUser._id, token, user: userData });
-    } catch (err) {
-      res.status(500).json({ message: "Xatolik: " + err.message });
+      res.status(201).json({
+        message: "Ro‘yxatdan o‘tish muvaffaqiyatli",
+        userId: newUser._id.toString(),
+        token,
+        user: userData
+      });
+    } catch (error) {
+      res.status(500).json({ message: "Serverda xatolik: " + error.message });
     }
   },
 
   login: async (req, res) => {
     try {
       const { email, password } = req.body;
-
+      
       if (!email || !password) {
-        return res.status(400).json({ message: "Barcha maydonlarni to‘ldiring" });
+        return res.status(403).send({ message: "Please fill all fields" });
       }
-
-      const user = await User.findOne({ email }).select('+password');
-      if (!user) return res.status(400).json({ message: "Email yoki parol noto‘g‘ri" });
-
+      
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).send({ message: "Invalid email" });
+      }
+      
       const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) return res.status(400).json({ message: "Email yoki parol noto‘g‘ri" });
-
-      const token = JWT.sign(
-        { id: user._id, role: user.role },
-        process.env.JWT_SECRET_KEY || 'secret',
-        { expiresIn: '12h' }
-      );
-
-      const userData = user.toObject();
-      delete userData.password;
-
-      res.status(200).json({ message: 'Kirish muvaffaqiyatli', userId: user._id, token, user: userData });
-    } catch (err) {
-      res.status(500).json({ message: "Xatolik: " + err.message });
+      if (!isMatch) {
+        return res.status(400).send({ message: "Invalid password" });
+      }
+      
+      const { password: pw, ...userData } = user._doc;
+      
+      const token = JWT.sign(userData, JWT_SECRET_KEY, { expiresIn: "48h" });
+      
+      res.status(200).send({
+        message: "Login successful",
+        user: userData,
+        token
+      });
+      
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ message: error.message });
     }
   }
 };
